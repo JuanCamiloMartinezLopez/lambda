19f354b667332d985b1d7a41c0fb6c60cf6afba2
JuanCamiloMartinezLopez <42418412+JuanCamiloMartinezLopez@users.noreply.github.com>
2019-06-12 15:55:03 -0500

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -15,6 +15,9 @@ def MaxofList(L):
 def MinofList(L):
     return min(L)
 
+def listaSuper(L):
+    return [x for x in L if superpares(x)==True]
+
 def NumberTriangular(num):
     return ((num*(num+1))/2)
 
@@ -22,7 +25,7 @@ def CabezaCola(l):
     return l[0],l[-1]
 
 l=[123,224,313,426,512,648]
-l2=[[1,3,5,8,48],[2,4,6,9,11],[4,5,7,10,12],[5,7,9,12,13]]
+l2=[[1,3,5,8,48],[2,4,6,9,11],[4,5,7,10,12],[22,5,7,9,12,13]]
 l3=[4,1,2,3,1024]
 l4=[(1,3),(3,2),(21,6),(15,5)]
 l6=[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
@@ -35,7 +38,7 @@ print(list(filter(lambda x: superpares(x)==True,l)))
 print("3.maximos de lista de listas")
 print(list(map(lambda x: MaxofList(x), l2)))
 print("4.minimos de lista de listas que es superpares")
-print(list(filter(lambda x:superpares(x)==True,list(map(lambda x: MinofList(x),l2)))))
+print(list(map(lambda x: MinofList(x),list(map(lambda x:listaSuper(x),l2)))))
 print("5.menos a la potencia de 5 de la cabeza")
 print(list(filter(lambda x: x<pow(l3[0],5),l3)))
 print("6.suma numero triangular")
